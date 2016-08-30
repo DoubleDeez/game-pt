7b96ce89062183efb81639e415857c4b51a50b54
Dylan Dumesnil <dadumesnil@gmail.com>
2016-08-29 19:22:11 -0700

    Setup character to use kinematic body with proper camera following

--- scripts/GameRoot.gd
+++ scripts/GameRoot.gd
@@ -6,19 +6,26 @@ export(NodePath) var CameraNodePath
 export(NodePath) var CharacterNodePath
 
 var TerrainNode
+var CameraNode
+var CharacterNode
 
 func _ready():
 	if AreExportVarsValid():
 		InitGame()
 	else:
 		print("GameRoot::_ready() - Export vars are invalid")
+		
+func _process(delta):
+	CameraNode.set_pos(CharacterNode.get_pos())
 
 func InitGame():
 	GenerateTerrain()
-	var CameraNode = get_node(CameraNodePath)
+	CameraNode = get_node(CameraNodePath)
 	CameraNode.set_pos(TerrainNode.GetTerrainCenter())
-	var CharacterNode = get_node(CharacterNodePath)
+	CameraNode.set_enable_follow_smoothing(true)
+	CharacterNode = get_node(CharacterNodePath)
 	CharacterNode.set_pos(TerrainNode.GetTerrainCenter())
+	set_process(true)
 
 func GenerateTerrain():
 	TerrainNode = get_node(TerrainNodePath)
--- scripts/George.gd
+++ scripts/George.gd
@@ -1,46 +1,79 @@
 
-extends AnimatedSprite
+extends KinematicBody2D
 
 export var MoveSpeed = 50
+export var AnimationFPS = 5
+export(NodePath) var CharacterNode
+
+const DIRECTION = {"S":0, "W":1, "N":2, "E":3}
 
 var DeltaPos = Vector2(0, 0)
+var CharacterDirection = DIRECTION.S
+var IsCharacterMoving = false
+var SpriteAnimationFrame = 0
+var CharacterSprite
+var TimeSinceFrameUpdate = 0
 
 func _ready():
-    set_process(true)
+	if CharacterNode != null:
+		CharacterSprite = get_node(CharacterNode)
+	else:
+		print("Geroge::_ready - CharacterNode is null")
+	set_process(true)
 
 func _process(delta):
+	TimeSinceFrameUpdate += delta
 	var PrevDeltaPos = DeltaPos
 	DeltaPos = Vector2(0, 0)
 	ProcessInput()
-	ProcessIdleState(PrevDeltaPos)
-	
-	set_pos(get_pos() + DeltaPos * delta)
+	ProcessMovement(delta)
+	if DeltaPos.length_squared() != 0 || PrevDeltaPos.length_squared() != 0:
+		DetermineDirection(PrevDeltaPos)
+		ProcessAnimation(delta)
 	
 func ProcessInput():
 	if Input.is_action_pressed("character_move_up"):
-		print("up")
 		DeltaPos.y -= MoveSpeed
-		play("Up")
 	if Input.is_action_pressed("character_move_down"):
-		print("down")
 		DeltaPos.y += MoveSpeed
-		play("Down")
 	if Input.is_action_pressed("character_move_left"):
-		print("left")
 		DeltaPos.x -= MoveSpeed
-		play("Left")
 	if Input.is_action_pressed("character_move_right"):
-		print("right")
 		DeltaPos.x += MoveSpeed
-		play("Right")
+	
+func ProcessMovement(delta):
+	var motion = move(DeltaPos * delta)
+	if (is_colliding()):
+		var normal = get_collision_normal()
+		motion = normal.slide(motion)
+		DeltaPos = normal.slide(DeltaPos)
+		move(motion)
 		
-func ProcessIdleState(PrevDeltaPos):
-	if DeltaPos.length_squared() == 0:
-		if PrevDeltaPos.y < 0:
-			play("Up_idle")
-		if PrevDeltaPos.y > 0:
-			play("Down_idle")
-		if PrevDeltaPos.x < 0:
-			play("Left_idle")
-		if PrevDeltaPos.x > 0:
-			play("Right_idle")
+func DetermineDirection(PrevDeltaPos):
+	var DirectionVector = DeltaPos
+	IsCharacterMoving = true
+	# determine if moving
+	if DirectionVector.length_squared() == 0:
+		DirectionVector = PrevDeltaPos
+		IsCharacterMoving = false
+	# Set direction based on DirectionVector
+	if DirectionVector.x > 0:
+		CharacterDirection = DIRECTION.E
+	elif DirectionVector.y < 0:
+		CharacterDirection = DIRECTION.N
+	elif DirectionVector.x < 0:
+		CharacterDirection = DIRECTION.W
+	elif DirectionVector.y > 0:
+		CharacterDirection = DIRECTION.S
+	
+func ProcessAnimation(delta):
+	if CharacterSprite != null:
+		if IsCharacterMoving:
+			if TimeSinceFrameUpdate >= (1.0/AnimationFPS):
+				SpriteAnimationFrame = (SpriteAnimationFrame + 1) % CharacterSprite.get_vframes()
+				TimeSinceFrameUpdate = 0
+		else:
+			SpriteAnimationFrame = 0
+		CharacterSprite.set_frame(CharacterDirection + SpriteAnimationFrame * CharacterSprite.get_vframes())
+	else:
+		print("Geroge::ProcessAnimation - CharacterSprite is null")
