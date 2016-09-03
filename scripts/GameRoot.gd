61ce2aaabc385bd70afcebeb6ba477d4f7f4eaba
Dylan Dumesnil <dadumesnil@gmail.com>
2016-09-03 01:26:51 -0700

    Terrain is tilemap based and added collision to lava section of map

--- scripts/GameRoot.gd
+++ scripts/GameRoot.gd
@@ -1,11 +1,9 @@
 
 extends Node
 
-export(NodePath) var TerrainNodePath
 export(NodePath) var CameraNodePath
 export(NodePath) var CharacterNodePath
 
-var TerrainNode
 var CameraNode
 var CharacterNode
 
@@ -19,19 +17,11 @@ func _process(delta):
 	CameraNode.set_pos(CharacterNode.get_pos())
 
 func InitGame():
-	GenerateTerrain()
 	CameraNode = get_node(CameraNodePath)
-	CameraNode.set_pos(TerrainNode.GetTerrainCenter())
 	CameraNode.set_enable_follow_smoothing(true)
 	CharacterNode = get_node(CharacterNodePath)
-	CharacterNode.set_pos(TerrainNode.GetTerrainCenter())
 	set_process(true)
 
-func GenerateTerrain():
-	TerrainNode = get_node(TerrainNodePath)
-	TerrainNode.GenerateTerrain()
-
 func AreExportVarsValid():
-	return TerrainNodePath != null \
-		&& CameraNodePath != null \
+	return CameraNodePath != null \
 		&& CharacterNodePath != null
